459b6a8f8c539b996d1ab7d82107fb55e998e53d
Kyrylo Gerasymenko <kyrylo.gerasymenko@globallogic.com>
2019-01-15 15:51:40 +0200

    moves view files into corresponding component directories

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ var usersRouter = require('./components/users/router');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// this option is for res.render() in .js
+app.set('views', path.join(__dirname, 'components'));
+// this option is for 'extends' in .jade
+app.locals.basedir = path.join(__dirname, 'components');
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -35,7 +38,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error/error');
 });
 
 module.exports = app;
--- components/home/router.js
+++ components/home/router.js
@@ -1,8 +1,9 @@
 var router = require('express').Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render(path.join(__dirname, 'index'), { title: 'Express' });
 });
 
 module.exports = router;
